c12b02500b0eb5bd45e36af2ab8aba1965784a1c
Tucker Leach <leachtucker@gmail.com>
2020-09-30 10:33:46 -0700

    Implemented usePrefersDarkmode in the useDarkMode hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,9 +1,19 @@
 
-import './useLocalStorage'
-import useLocalStorage from './useLocalStorage';
+import { useMedia } from 'use-media'
+import useLocalStorage from './useLocalStorage'
 
-const useDarkMode = (value) => {
-    const [isDarkmode, setDarkmode] = useLocalStorage('isDarkmode', value);
+const usePrefersDarkmode = () => {
+    return useMedia(['(perfers-color-scheme: dark'], [true], false)
+}
+
+const useDarkMode = () => {
+    const [isDarkmode, setDarkmode] = useLocalStorage('isDarkmode', false);
+
+    // Finding OS preferences for dark/light mode
+    const prefersDarkmode = usePrefersDarkmode();
+
+    // Setting that value to state
+    setDarkmode(prefersDarkmode);
 
     return [isDarkmode, setDarkmode];
 }
